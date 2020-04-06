bcd3dd60055e7f9868f2b21ed6400e03935e888e
music-brain88 <music.brain88@gmail.com>
2020-04-06 23:25:15 +0900

    初めてのrust

---
+++ sample/sample.rs
@@ -0,0 +1,14 @@
+fn main() {
+    const PI: i32 = 3;
+    let radius = 10;
+
+    // 円周率の計算
+    let cir = 2 * PI * radius;
+    println!("円周率 = {}", cir);
+
+
+    // 面積の計算
+    let area = PI * radius * radius;
+    println!("面積 {}", area);
+
+}
