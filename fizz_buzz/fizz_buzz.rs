d6b888432abeb0c00693e9e9072e59679b5a81df
music-brain88 <music.brain88@gmail.com>
2020-04-12 12:48:32 +0900

    simple fizz buzz

--- fizz_buzz/fizz_buzz.rs
+++ fizz_buzz/fizz_buzz.rs
@@ -1,14 +1,12 @@
 fn main() {
     println!("FizzBuzz game");
-    for i in 1..101 {
-        if i % 3 == 0 && i % 5 == 0 {
-            println!("{} is FizzBuzz", i);
-        } else if i%3 == 0 {
-            println!("{} is Fizz", i);
-        } else if i%5 == 0 {
-            println!("{} is Buzz", i);
-        } else {
-            println!("{}", i)
-        }
+    for i in 1..10001 {
+       let x: String; 
+       println!("{}", match i%15 {
+            0 => "FizzBuzz",
+            3|6|9|12 => "Fizz",
+            5|10 => "Buzz",
+            _ => {x=i.to_string(); &x},
+       });
     }
 }
