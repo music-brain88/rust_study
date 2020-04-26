f65493f63b3553d07288abe7dfc5528494d2cff9
music-brain88 <music.brain88@gmail.com>
2020-04-26 23:14:28 +0900

    learn vector

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).ok();
+    let sample: Vec<i32> = s.trim().split_whitespace().map(|e| e.parse().ok().unwrap()).collect();
+
+    println!("sample: {:?}", sample);
 }
